d4681fab0651076148131aa052e9ffb316762348
Akshith Kandivanam <81925146+akkik04@users.noreply.github.com>
2021-07-16 12:56:41 -0400

    Added optimal temp detection.
    Software can now detect the optimal temp in a room and flash signals if it is too hot or too cold.

--- TempAndHumidityDetector.ino
+++ TempAndHumidityDetector.ino
@@ -1,3 +1,9 @@
+  /*
+   * A FEW CLARIFICATIONS FOR THIS PROJECT:
+   * 
+   *  - OPTIMAL ROOM TEMPERATURE RANGE FOR THIS PROJECT IS BETWEEN 18.0°C and 25.0°C.
+   */
+    
   // importing the required module to operate the LCD.
   #include <LiquidCrystal.h>
   
@@ -12,6 +18,10 @@
 
 // SETUP.
 void setup() {
+
+  // declaring the pins for optimal temperature range detection.
+  pinMode(13, OUTPUT);
+  pinMode(2, OUTPUT);
   
   // initializing the DHT.
   dht.begin();
@@ -43,6 +53,47 @@ void loop() {
 
   // if no error is found we start the printing process at the first row.
   else {
+
+    // creating a nested if-statement to check if the temperature is below the optimal range.
+    if (temperature < 18.0) {
+
+      // flashing the 'blue' LED to indicate that the room temp is too cold.
+      digitalWrite(2, HIGH);
+      delay(100);
+      digitalWrite(2, LOW);
+      delay(100);
+
+      // code to print that the temperature is below optimal range on the LCD.
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("   Too cold!");
+    }
+
+    // checking if the temperature is above the optimal range.
+    else if (temperature > 25.0) {
+
+      // flashing the 'red' LED to indicate that the room temp is too hot.
+      digitalWrite(13, HIGH);
+      delay(100);
+      digitalWrite(13, LOW);
+      delay(100);
+
+      // code to print that the temperature is above optimal range on the LCD.
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("    Too hot!");
+    }
+
+    // code to let the user know that they are within the optimal range if conditions are met.
+    else {
+
+      lcd.setCursor(0, 0);
+      lcd.print(" Optimal Temp");
+    }
+
+    // adding a delay for LCD effect.
+    delay(1000);
+
     lcd.setCursor(0, 0);
 
     // printing the temperature with the '°' character.
@@ -56,5 +107,6 @@ void loop() {
     lcd.print("Humidity: ");
     lcd.print(humidity);
     lcd.print("%");
+    
   }
 }
